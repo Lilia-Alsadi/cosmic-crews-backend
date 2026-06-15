1d707464d3e75aca1467850838bcf61337157db5
Lilia Alsadi <22110154@htu.edu.jo>
2026-06-15 21:15:45 +0300

    fixed server

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-const authRoutes = require('./routes/auth-routes');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const crewRoutes = require('./routes/crewRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/crews', crewRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
